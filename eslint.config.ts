a289b3569d6e5e190b3a7ab2d71a6476256d8319
Carina Ruut <carinaruut@gmail.com>
2026-08-18 19:37:37 +0300

    add eslint

---
+++ eslint.config.ts
@@ -0,0 +1,39 @@
+import eslint from '@eslint/js'
+import vue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+import vueParser from 'vue-eslint-parser'
+
+export default tseslint.config(
+  {
+    ignores: ['dist/**', 'node_modules/**']
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tseslint.parser,
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    }
+  },
+  {
+    files: ['**/*.{ts,tsx,vue}'],
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_'
+        }
+      ],
+      '@typescript-eslint/no-explicit-any': 'error',
+      'vue/multi-word-component-names': 'off'
+    }
+  }
+)
